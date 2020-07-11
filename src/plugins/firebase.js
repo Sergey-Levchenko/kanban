ddbfac12ae193c0d44c01aab9ae984bf5762289e
SergeyLevchenko <splendorsmile@gmail.com>
2020-07-11 20:16:06 +0300

    add key

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -5,19 +5,10 @@ import 'firebase/messaging';
 import 'firebase/storage';
 import 'firebase/auth';
 import store from '../store';
-// import keys from '../keys/index';
-
-console.log('firebase:', process.env);
-// console.log('firebase:', keys);
-console.log('firebase:', process.env.FIREBASE_API_KEY);
-
-let key;
-if (process.env.NODE_ENV === 'production') key = process.env.FIREBASE_API_KEY;
-else key = 'AIzaSyAAV3J0g03gcWSyqCNMn-NFTLhVvxvthBk';
 
 if (!fb.apps.length) {
   fb.initializeApp({
-    apiKey: key,
+    apiKey: 'AIzaSyAAV3J0g03gcWSyqCNMn-NFTLhVvxvthBk',
     authDomain: 'honey-s.firebaseapp.com',
     databaseURL: 'https://honey-s.firebaseio.com',
     projectId: 'honey-s',
